975c766ba9f7a1171c497d9c932b01dcfa89dd7b
pesc3485 <pesc3485@colorado.edu>
2025-11-03 19:37:35 -0700

    Upgrading dependencies to work with current python version

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-import psycopg2
+import psycopg
 
 app = Flask(__name__)
 
@@ -9,6 +9,6 @@ def hello_world():
 
 @app.route('/db_test')
 def testing():
-    conn = psycopg2.connect("postgresql://flask_postgres_pesc3485_user:lbcH2gmgGSjMYwgEoLQGrSKVv4kUsnLp@dpg-d44m7ti4d50c73ek31mg-a.oregon-postgres.render.com/flask_postgres_pesc3485")
+    conn = psycopg.connect("postgresql://flask_postgres_pesc3485_user:lbcH2gmgGSjMYwgEoLQGrSKVv4kUsnLp@dpg-d44m7ti4d50c73ek31mg-a.oregon-postgres.render.com/flask_postgres_pesc3485")
     conn.close()
     return "Database Cponnected Successfully"
