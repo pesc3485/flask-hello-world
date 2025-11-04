91271e6277860e663d2a6fe16faf85a4127da4f8
pesc3485 <pesc3485@colorado.edu>
2025-11-03 19:49:21 -0700

    /db_create route created - testing creation of basketball table

--- app.py
+++ app.py
@@ -11,4 +11,21 @@ def hello_world():
 def testing():
     conn = psycopg.connect("postgresql://flask_postgres_pesc3485_user:lbcH2gmgGSjMYwgEoLQGrSKVv4kUsnLp@dpg-d44m7ti4d50c73ek31mg-a.oregon-postgres.render.com/flask_postgres_pesc3485")
     conn.close()
-    return "Database Cponnected Successfully"
+    return "Database Connected Successfully"
+
+@app.route('/db_create')
+def create():
+    conn = psycopg.connect("postgresql://flask_postgres_pesc3485_user:lbcH2gmgGSjMYwgEoLQGrSKVv4kUsnLp@dpg-d44m7ti4d50c73ek31mg-a.oregon-postgres.render.com/flask_postgres_pesc3485")
+    cur = conn.cursor()
+    cur.execute('''
+                CREATE TABLE IF NOT EXISTS Basketball(
+                First VARCHAR(255),
+                Last VARCHAR(255),
+                City VARCHAR(255),
+                Name VARCHAR(255),
+                Number INT                
+                );               
+                ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created."
