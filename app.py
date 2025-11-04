03460ea82fedd1befcd2ee2511842ba9c7d13977
pesc3485 <pesc3485@colorado.edu>
2025-11-03 19:28:46 -0700

    Initial Commit With DB Testing

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import psycopg2
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello World from Peter Scott in 330'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgresql://flask_postgres_pesc3485_user:lbcH2gmgGSjMYwgEoLQGrSKVv4kUsnLp@dpg-d44m7ti4d50c73ek31mg-a.oregon-postgres.render.com/flask_postgres_pesc3485")
+    conn.close()
+    return "Database Cponnected Successfully"
